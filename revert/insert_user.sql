c6b06ca386d3de7bd892040c99c74561155a2338
Jason Kumpf <jason@kumpf.io>
2015-08-31 14:55:37 -0400

    Rework

--- revert/insert_user.sql
+++ revert/insert_user.sql
@@ -1,7 +1,23 @@
--- Revert flipr:insert_user from mysql
+-- Deploy flipr:insert_user to mysql
+-- requires: users
+-- requires: appuser
 
 BEGIN;
 
-DROP PROCEDURE insert_user;
+DELIMITER //
+ 
+CREATE PROCEDURE insert_user(
+    nickname VARCHAR(512),
+    password VARCHAR(512)
+) SQL SECURITY DEFINER
+BEGIN
+    INSERT INTO users (nickname, password, timestamp)
+    VALUES (nickname, md5(password), UTC_TIMESTAMP(6));
+END
+//
+ 
+DELIMITER ;
+ 
+GRANT EXECUTE ON PROCEDURE insert_user to flipr;
 
 COMMIT;
